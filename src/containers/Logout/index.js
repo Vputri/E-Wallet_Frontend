f93daa3ff73e40d8c0f42f5e50c9bc9b20cf7b4d
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2022-03-18 13:21:43 +0700

    first commit

--- src/containers/Logout/index.js
+++ src/containers/Logout/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axiosInstance from '../../axios';
 import { useHistory } from 'react-router-dom';
 
@@ -8,11 +8,11 @@ function Logout() {
 	useEffect(() => {
 		const response = axiosInstance.post('user/logout/blacklist/', {
 			refresh_token: localStorage.getItem('refresh_token'),
-		})
+		});
 		localStorage.removeItem('access_token');
 		localStorage.removeItem('refresh_token');
 		axiosInstance.defaults.headers['Authorization'] = null;
-		history.push('login');
+		history.push('/login');
 	});
 	return <div>Logout</div>;
 }
